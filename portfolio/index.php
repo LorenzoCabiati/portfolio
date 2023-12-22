eb09032ed1f3cd3b49a6cb6aabeb4e3b7a46a69c
LorenzoCabiati <154637824+LorenzoCabiati@users.noreply.github.com>
2023-12-22 22:54:21 +0100

    Add files via upload

---
+++ portfolio/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php include 'conf.php' ?>
+  <title>Cabiati Lorenzo</title>
+</head>
+
+<body>
+
+  <?php include 'header.php' ?>
+
+  <div class="content-container">
+    <div class="text-content">
+      <div class="title">
+        Cabiati Lorenzo
+      </div>
+      <div class="content">
+        Junior Web developer e studente ICT
+      </div>
+      <div class="content-button">
+
+        <a class="button-contact" href="contattami.php" >Contattami</a>
+        
+      </div>
+    </div>
+    <div class="image-container">
+      <img src="image1.png" alt="">
+    </div>
+  </div>
+  
+
+
+  <?php include 'footer.php' ?>
+</body>
+
+</html>
